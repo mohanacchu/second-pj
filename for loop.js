d81e52be343098296a5906d2d63ffa1661f2767d
Mohan <33092062+mohanacchu@users.noreply.github.com>
2018-07-20 16:15:28 +0530

    forEach function added

---
+++ for loop.js
@@ -0,0 +1,8 @@
+//forEach
+var myArray = [10,20,'hello',{fname:'mohan',lname:'gowda',age:23}];
+myArray.forEach(function(item,index,array){
+  console.log(item,index)
+console.log("hello");
+  console.log(array);
+  
+});
