3a257e6ce4b87199078eb52e500c1a9407da0e5f
mohanacchu <mohangowda1994@gmail.com>
2021-03-09 08:39:50 +0530

    quiz template added

---
+++ quiz/quiz.js
@@ -0,0 +1,26 @@
+let questionDirectory = [
+    {
+        questions: "Capital of Karnataka ?",
+        a: "Mysore",
+        b: "Mandya",
+        c: "Belagavi",
+        d: "Bengaluru",
+        correctAnswer: "d"
+    },
+    {
+        questions: "11 times French open winner ?",
+        a: "Rafael Nadal",
+        b: "Murry",
+        c: "Federer",
+        d: "Williamson",
+        correctAnswer: "a"
+    },
+    {
+        questions: "Mission Impossible Lead Actor ?",
+        a: "Brad pitt",
+        b: "Tom Cruose",
+        c: "Will Smith",
+        d: "Robert Downey Jr",
+        correctAnswer: "b"
+    }
+]
